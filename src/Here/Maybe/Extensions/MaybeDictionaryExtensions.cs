155c627380e7d7ab196aaa3a5de78965fd7e9422
Alexandre Rabérin <kernelith@live.fr>
2018-07-29 17:10:55 +0200

    Extensions to try get value from a dictionary always return None when the key is null.
    Add extensions to try get a typed value in dictionary of object.

--- src/Here/Maybe/Extensions/MaybeDictionaryExtensions.cs
+++ src/Here/Maybe/Extensions/MaybeDictionaryExtensions.cs
@@ -11,8 +11,11 @@ namespace Here.Maybes.Extensions
     {
         // Helper method to try get a value
         [Pure]
-        private static Maybe<TValue> TryGetValue<TKey, TValue>(MaybeTryGetExtensions.TryGet<TKey, TValue> tryGetFunc, [NotNull] TKey key)
+        private static Maybe<TValue> TryGetValue<TKey, TValue>(MaybeTryGetExtensions.TryGet<TKey, TValue> tryGetFunc, [CanBeNull] TKey key)
         {
+            if (key == null)
+                return Maybe<TValue>.None;
+
             var getter = MaybeTryGetExtensions.CreateGet(tryGetFunc);
             return getter(key);
         }
@@ -26,9 +29,26 @@ namespace Here.Maybes.Extensions
         /// <param name="key">Searched key.</param>
         /// <returns><see cref="Maybe{TValue}"/> that wrap the result of the get.</returns>
         [PublicAPI, Pure]
-        public static Maybe<TValue> TryGetValue<TKey, TValue>([NotNull] this IDictionary<TKey, TValue> dictionary, [NotNull] TKey key)
+        public static Maybe<TValue> TryGetValue<TKey, TValue>([NotNull] this IDictionary<TKey, TValue> dictionary, [CanBeNull] TKey key)
         {
             return TryGetValue<TKey, TValue>(dictionary.TryGetValue, key);
+        }
+
+        /// <summary>
+        /// Try to get the value for the given key in the dictionary.
+        /// </summary>
+        /// <typeparam name="TKey"><see cref="Type"/> of this <see cref="IDictionary{TKey, Object}"/> key.</typeparam>
+        /// <typeparam name="TValue"><see cref="Type"/> of the expected value.</typeparam>
+        /// <param name="dictionary"><see cref="IDictionary{TKey, Object}"/> on which performing treatment.</param>
+        /// <param name="key">Searched key.</param>
+        /// <returns><see cref="Maybe{TValue}"/> that wrap the result of the get.</returns>
+        [PublicAPI, Pure]
+        public static Maybe<TValue> TryGetValue<TKey, TValue>([NotNull] this IDictionary<TKey, object> dictionary, [CanBeNull] TKey key)
+        {
+            var objectValue = dictionary.TryGetValue(key);
+            if (objectValue.HasValue && objectValue.Value is TValue expectedValue)
+                return Maybe<TValue>.Some(expectedValue);
+            return Maybe<TValue>.None;
         }
 
         /// <summary>
@@ -40,9 +60,26 @@ namespace Here.Maybes.Extensions
         /// <param name="key">Searched key.</param>
         /// <returns><see cref="Maybe{TValue}"/> that wrap the result of the get.</returns>
         [PublicAPI, Pure]
-        public static Maybe<TValue> TryGetReadonlyValue<TKey, TValue>([NotNull] this IReadOnlyDictionary<TKey, TValue> dictionary, [NotNull] TKey key)
+        public static Maybe<TValue> TryGetReadonlyValue<TKey, TValue>([NotNull] this IReadOnlyDictionary<TKey, TValue> dictionary, [CanBeNull] TKey key)
         {
             return TryGetValue<TKey, TValue>(dictionary.TryGetValue, key);
+        }
+
+        /// <summary>
+        /// Try to get the value for the given key in the dictionary.
+        /// </summary>
+        /// <typeparam name="TKey"><see cref="Type"/> of this <see cref="IReadOnlyDictionary{TKey, Object}"/> key.</typeparam>
+        /// <typeparam name="TValue"><see cref="Type"/> of the expected value.</typeparam>
+        /// <param name="dictionary"><see cref="IReadOnlyDictionary{TKey, Object}"/> on which performing treatment.</param>
+        /// <param name="key">Searched key.</param>
+        /// <returns><see cref="Maybe{TValue}"/> that wrap the result of the get.</returns>
+        [PublicAPI, Pure]
+        public static Maybe<TValue> TryGetReadonlyValue<TKey, TValue>([NotNull] this IReadOnlyDictionary<TKey, object> dictionary, [CanBeNull] TKey key)
+        {
+            var objectValue = dictionary.TryGetReadonlyValue(key);
+            if (objectValue.HasValue && objectValue.Value is TValue expectedValue)
+                return Maybe<TValue>.Some(expectedValue);
+            return Maybe<TValue>.None;
         }
     }
 }
