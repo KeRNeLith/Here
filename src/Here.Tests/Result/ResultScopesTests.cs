dc3e7ffe500d40037f4f70b485ff0c36b6f6ff76
Alexandre Rabérin <kernelith@live.fr>
2018-07-31 00:48:49 +0200

    Add scopes to safely return any type of Result.

---
+++ src/Here.Tests/Result/ResultScopesTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using NUnit.Framework;
+using Here.Results;
+
+namespace Here.Tests.Results
+{
+    /// <summary>
+    /// Tests for <see cref="Result"/>, <see cref="Result{T}"/>, <see cref="CustomResult{TError}"/> and <see cref="Result{T, TError}"/> safe scopes.
+    /// </summary>
+    [TestFixture]
+    internal class ResultScopesTests : ResultTestsBase
+    {
+        [Test]
+        public void ResultSafeScope()
+        {
+            Result result = ResultScope.SafeResult(Result.Ok);
+            CheckResultOk(result);
+
+            result = ResultScope.SafeResult(() => Result.Warn("My warning"));
+            CheckResultWarn(result, "My warning");
+
+            result = ResultScope.SafeResult(() => Result.Fail("My failure"));
+            CheckResultFail(result, "My failure");
+
+            var thrownException = new Exception("My exception");
+            result = ResultScope.SafeResult(() => throw thrownException);
+            CheckResultFail(result, string.Format(ResultScope.ResultScopeErrorMessage, "My exception"), thrownException);
+        }
+
+        [Test]
+        public void ValueResultSafeScope()
+        {
+            Result<int> result = ResultScope.SafeValueResult(() => Result.Ok(12));
+            CheckResultOk(result, 12);
+
+            result = ResultScope.SafeValueResult(() => Result.Warn(42, "My warning"));
+            CheckResultWarn(result, 42, "My warning");
+
+            result = ResultScope.SafeValueResult(() => Result.Fail<int>("My failure"));
+            CheckResultFail(result, "My failure");
+
+            var thrownException = new Exception("My exception");
+            result = ResultScope.SafeValueResult<int>(() => throw thrownException);
+            CheckResultFail(result, string.Format(ResultScope.ValueResultScopeErrorMessage, "My exception"), thrownException);
+        }
+
+        [Test]
+        public void CustomResultSafeScope()
+        {
+            var defaultError = new CustomErrorTest { ErrorCode = 51 };
+            CustomResult<CustomErrorTest> result = ResultScope.SafeCustomResult(Result.CustomOk<CustomErrorTest>, () => defaultError);
+            CheckResultOk(result);
+
+            result = ResultScope.SafeCustomResult(Result.CustomOk<CustomErrorTest>, defaultError);
+            CheckResultOk(result);
+
+            result = ResultScope.SafeCustomResult(() => Result.CustomWarn<CustomErrorTest>("My warning"), () => defaultError);
+            CheckResultWarn(result, "My warning");
+
+            result = ResultScope.SafeCustomResult(() => Result.CustomWarn<CustomErrorTest>("My warning"), defaultError);
+            CheckResultWarn(result, "My warning");
+
+            var errorObject = new CustomErrorTest { ErrorCode = 12 };
+            result = ResultScope.SafeCustomResult(() => Result.CustomFail("My failure", errorObject), () => defaultError);
+            CheckResultFail(result, "My failure", errorObject);
+
+            result = ResultScope.SafeCustomResult(() => Result.CustomFail("My failure", errorObject), defaultError);
+            CheckResultFail(result, "My failure", errorObject);
+
+            var thrownException = new Exception("My exception");
+            result = ResultScope.SafeCustomResult(() => throw thrownException, () => defaultError);
+            CheckResultFail(result, string.Format(ResultScope.CustomResultScopeErrorMessage, "My exception"), defaultError, thrownException);
+
+            result = ResultScope.SafeCustomResult(() => throw thrownException, defaultError);
+            CheckResultFail(result, string.Format(ResultScope.CustomResultScopeErrorMessage, "My exception"), defaultError, thrownException);
+        }
+
+        [Test]
+        public void ValueCustomResultSafeScope()
+        {
+            var defaultError = new CustomErrorTest { ErrorCode = 51 };
+            Result<int, CustomErrorTest> result = ResultScope.SafeValueCustomResult(() => Result.Ok<int, CustomErrorTest>(42), () => defaultError);
+            CheckResultOk(result, 42);
+
+            result = ResultScope.SafeValueCustomResult(() => Result.Ok<int, CustomErrorTest>(12), defaultError);
+            CheckResultOk(result, 12);
+
+            result = ResultScope.SafeValueCustomResult(() => Result.Warn<int, CustomErrorTest>(21, "My warning"), () => defaultError);
+            CheckResultWarn(result, 21, "My warning");
+
+            result = ResultScope.SafeValueCustomResult(() => Result.Warn<int, CustomErrorTest>(51, "My warning"), defaultError);
+            CheckResultWarn(result, 51, "My warning");
+
+            var errorObject = new CustomErrorTest { ErrorCode = 12 };
+            result = ResultScope.SafeValueCustomResult(() => Result.Fail<int, CustomErrorTest>("My failure", errorObject), () => defaultError);
+            CheckResultFail(result, "My failure", errorObject);
+
+            result = ResultScope.SafeValueCustomResult(() => Result.Fail<int, CustomErrorTest>("My failure", errorObject), defaultError);
+            CheckResultFail(result, "My failure", errorObject);
+
+            var thrownException = new Exception("My exception");
+            result = ResultScope.SafeValueCustomResult<int, CustomErrorTest>(() => throw thrownException, () => defaultError);
+            CheckResultFail(result, string.Format(ResultScope.ValueCustomResultScopeErrorMessage, "My exception"), defaultError, thrownException);
+
+            result = ResultScope.SafeValueCustomResult<int, CustomErrorTest>(() => throw thrownException, defaultError);
+            CheckResultFail(result, string.Format(ResultScope.ValueCustomResultScopeErrorMessage, "My exception"), defaultError, thrownException);
+        }
+    }
+}
---
+++ src/Here/Result/ResultScopes.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Here.Results
+{
+    /// <summary>
+    /// Scopes to safely return a <see cref="Result"/>, <see cref="Result{T}"/>, <see cref="CustomResult{TError}"/> or <see cref="Result{T,TError}"/>.
+    /// </summary>
+    [PublicAPI]
+    public static class ResultScope
+    {
+        #region Error messages
+
+        internal const string ResultScopeErrorMessage = "An exception occurred while running a Result scope\n" +
+                                                        "Error is: {0}";
+
+        internal const string ValueResultScopeErrorMessage = "An exception occurred while running a Result<T> scope\n" +
+                                                             "Error is: {0}";
+
+        internal const string CustomResultScopeErrorMessage = "An exception occurred while running a CustomResult<TError> scope\n" +
+                                                              "Error is: {0}";
+
+        internal const string ValueCustomResultScopeErrorMessage = "An exception occurred while running a Result<T, TError> scope\n" +
+                                                                   "Error is: {0}";
+
+        #endregion
+
+        /// <summary>
+        /// Run the given <paramref name="action"/> in a safe scope that always return a <see cref="Result"/>.
+        /// </summary>
+        /// <param name="action">Function to call.</param>
+        /// <returns>A <see cref="Result"/>.</returns>
+        [PublicAPI]
+        public static Result SafeResult([NotNull, InstantHandle] Func<Result> action)
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail(
+                    string.Format(ResultScopeErrorMessage, ex.Message), 
+                    ex);
+            }
+        }
+
+        /// <summary>
+        /// Run the given <paramref name="action"/> in a safe scope that always return a <see cref="Result{T}"/>.
+        /// </summary>
+        /// <param name="action">Function to call.</param>
+        /// <returns>A <see cref="Result{T}"/>.</returns>
+        [PublicAPI]
+        public static Result<T> SafeValueResult<T>([NotNull, InstantHandle] Func<Result<T>> action)
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail<T>(
+                    string.Format(ValueResultScopeErrorMessage, ex.Message),
+                    ex);
+            }
+        }
+
+        /// <summary>
+        /// Run the given <paramref name="action"/> in a safe scope that always return a <see cref="Result"/>.
+        /// </summary>
+        /// <param name="action">Function to call.</param>
+        /// <param name="errorObject">Error object to return in case an exception is thrown.</param>
+        /// <returns>A <see cref="CustomResult{TError}"/>.</returns>
+        [PublicAPI]
+        public static CustomResult<TError> SafeCustomResult<TError>([NotNull, InstantHandle] Func<CustomResult<TError>> action, [NotNull] TError errorObject)
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception ex)
+            {
+                return Result.CustomFail(
+                    string.Format(CustomResultScopeErrorMessage, ex.Message),
+                    errorObject,
+                    ex);
+            }
+        }
+
+        /// <summary>
+        /// Run the given <paramref name="action"/> in a safe scope that always return a <see cref="Result"/>.
+        /// </summary>
+        /// <param name="action">Function to call.</param>
+        /// <param name="errorFactory">Function to create a custom error objec in case an exception is thrown.</param>
+        /// <returns>A <see cref="CustomResult{TError}"/>.</returns>
+        [PublicAPI]
+        public static CustomResult<TError> SafeCustomResult<TError>([NotNull, InstantHandle] Func<CustomResult<TError>> action, [NotNull] Func<TError> errorFactory)
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception ex)
+            {
+                return Result.CustomFail(
+                    string.Format(CustomResultScopeErrorMessage, ex.Message),
+                    errorFactory(),
+                    ex);
+            }
+        }
+
+        /// <summary>
+        /// Run the given <paramref name="action"/> in a safe scope that always return a <see cref="Result"/>.
+        /// </summary>
+        /// <param name="action">Function to call.</param>
+        /// <param name="errorObject">Error object to return in case an exception is thrown.</param>
+        /// <returns>A <see cref="Result{T, TError}"/>.</returns>
+        [PublicAPI]
+        public static Result<T, TError> SafeValueCustomResult<T, TError>([NotNull, InstantHandle] Func<Result<T, TError>> action, [NotNull] TError errorObject)
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail<T, TError>(
+                    string.Format(ValueCustomResultScopeErrorMessage, ex.Message),
+                    errorObject,
+                    ex);
+            }
+        }
+
+        /// <summary>
+        /// Run the given <paramref name="action"/> in a safe scope that always return a <see cref="Result"/>.
+        /// </summary>
+        /// <param name="action">Function to call.</param>
+        /// <param name="errorFactory">Function to create a custom error objec in case an exception is thrown.</param>
+        /// <returns>A <see cref="Result{T, TError}"/>.</returns>
+        [PublicAPI]
+        public static Result<T, TError> SafeValueCustomResult<T, TError>([NotNull, InstantHandle] Func<Result<T, TError>> action, [NotNull] Func<TError> errorFactory)
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail<T, TError>(
+                    string.Format(ValueCustomResultScopeErrorMessage, ex.Message),
+                    errorFactory(),
+                    ex);
+            }
+        }
+    }
+}
