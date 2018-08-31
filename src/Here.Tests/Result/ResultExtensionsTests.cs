a6fc91c73f13e60aa09daa7196e51f14b8318fbb
Alexandre Rabérin <kernelith@live.fr>
2018-08-31 22:32:00 +0200

    Add ensure extensions to results that allow to run a predicate based on a Result.

---
+++ src/Here.Tests/Result/ResultExtensionsTests.cs
@@ -0,0 +1,523 @@
+﻿using NUnit.Framework;
+using Here.Results;
+using Here.Results.Extensions;
+
+namespace Here.Tests.Results
+{
+    /// <summary>
+    /// Tests for <see cref="Result"/>, <see cref="Result{T}"/>, <see cref="CustomResult{TError}"/> and <see cref="Result{T, TError}"/> extensions.
+    /// </summary>
+    [TestFixture]
+    internal class ResultExtensionsTests : ResultTestsBase
+    {
+        [Test]
+        public void ResultEnsure()
+        {
+            string errorMessage = "My error message";
+            int counter = 0;
+            // Ok result
+            var ok = Result.Ok();
+
+            var result = ok.Ensure(
+                () =>
+                {
+                    ++counter;
+                    return true;
+                },
+                errorMessage);
+            Assert.AreEqual(1, counter);
+            CheckResultOk(result);
+
+            result = ok.Ensure(
+                () =>
+                {
+                    ++counter;
+                    return false;
+                },
+                errorMessage);
+            Assert.AreEqual(2, counter);
+            CheckResultFail(result, errorMessage);
+
+            // Warning result
+            var warning = Result.Warn("My warning");
+
+            result = warning.Ensure(
+                () =>
+                {
+                    ++counter;
+                    return true;
+                },
+                errorMessage);
+            Assert.AreEqual(3, counter);
+            CheckResultWarn(result, "My warning");
+
+            result = warning.Ensure(
+                () =>
+                {
+                    ++counter;
+                    return false;
+                },
+                errorMessage);
+            Assert.AreEqual(4, counter);
+            CheckResultFail(result, errorMessage);
+
+            // Failure result
+            var failure = Result.Fail("My failure");
+
+            result = failure.Ensure(
+                () =>
+                {
+                    ++counter;
+                    return true;
+                },
+                errorMessage);
+            Assert.AreEqual(4, counter);
+            CheckResultFail(result, "My failure");
+
+            result = failure.Ensure(
+                () =>
+                {
+                    ++counter;
+                    return false;
+                },
+                errorMessage);
+            Assert.AreEqual(4, counter);
+            CheckResultFail(result, "My failure");
+        }
+
+        [Test]
+        public void ValueResultEnsure()
+        {
+            string errorMessage = "My error message";
+            int counter = 0;
+            // Ok result
+            var ok = Result.Ok(42);
+
+            var result = ok.Ensure(
+                value =>
+                {
+                    ++counter;
+                    return true;
+                },
+                errorMessage);
+            Assert.AreEqual(1, counter);
+            CheckResultOk(result);
+
+            result = ok.Ensure(
+                value =>
+                {
+                    ++counter;
+                    return false;
+                },
+                errorMessage);
+            Assert.AreEqual(2, counter);
+            CheckResultFail(result, errorMessage);
+
+            // Warning result
+            var warning = Result.Warn(15, "My warning");
+
+            result = warning.Ensure(
+                value =>
+                {
+                    ++counter;
+                    return true;
+                },
+                errorMessage);
+            Assert.AreEqual(3, counter);
+            CheckResultWarn(result, "My warning");
+
+            result = warning.Ensure(
+                value =>
+                {
+                    ++counter;
+                    return false;
+                },
+                errorMessage);
+            Assert.AreEqual(4, counter);
+            CheckResultFail(result, errorMessage);
+
+            // Failure result
+            var failure = Result.Fail<int>("My failure");
+
+            result = failure.Ensure(
+                value =>
+                {
+                    ++counter;
+                    return true;
+                },
+                errorMessage);
+            Assert.AreEqual(4, counter);
+            CheckResultFail(result, "My failure");
+
+            result = failure.Ensure(
+                value =>
+                {
+                    ++counter;
+                    return false;
+                },
+                errorMessage);
+            Assert.AreEqual(4, counter);
+            CheckResultFail(result, "My failure");
+        }
+
+        [Test]
+        public void CustomResultEnsure()
+        {
+            string errorMessage = "My error message";
+            var customErrorObject = new CustomErrorTest { ErrorCode = -1 };
+            var customErrorObjectFactory = new CustomErrorTest { ErrorCode = -2 };
+            int counterPredicate = 0;
+            int counterErrorFactory = 0;
+            // Ok result
+            var ok = Result.CustomOk<CustomErrorTest>();
+
+            var result = ok.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(1, counterPredicate);
+            CheckResultOk(result);
+
+            result = ok.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(2, counterPredicate);
+            Assert.AreEqual(0, counterErrorFactory);
+            CheckResultOk(result);
+
+            result = ok.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(3, counterPredicate);
+            CheckResultFail(result, errorMessage, customErrorObjectFactory);
+
+            result = ok.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(4, counterPredicate);
+            Assert.AreEqual(1, counterErrorFactory);
+            CheckResultFail(result, errorMessage, customErrorObjectFactory);
+
+            // Warning result
+            var warning = Result.CustomWarn<CustomErrorTest>("My warning");
+
+            result = warning.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(5, counterPredicate);
+            CheckResultWarn(result, "My warning");
+
+            result = warning.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(6, counterPredicate);
+            Assert.AreEqual(1, counterErrorFactory);
+            CheckResultWarn(result, "My warning");
+
+            result = warning.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(7, counterPredicate);
+            CheckResultFail(result, errorMessage, customErrorObjectFactory);
+
+            result = warning.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(8, counterPredicate);
+            Assert.AreEqual(2, counterErrorFactory);
+            CheckResultFail(result, errorMessage, customErrorObjectFactory);
+
+            // Failure result
+            var failure = Result.CustomFail<CustomErrorTest>("My failure", customErrorObject);
+
+            result = failure.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(8, counterPredicate);
+            CheckResultFail(result, "My failure", customErrorObject);
+
+            result = failure.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(8, counterPredicate);
+            Assert.AreEqual(2, counterErrorFactory);
+            CheckResultFail(result, "My failure", customErrorObject);
+
+            result = failure.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(8, counterPredicate);
+            CheckResultFail(result, "My failure", customErrorObject);
+
+            result = failure.Ensure(
+                () =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(8, counterPredicate);
+            Assert.AreEqual(2, counterErrorFactory);
+            CheckResultFail(result, "My failure", customErrorObject);
+        }
+
+        [Test]
+        public void CustomValueResultEnsure()
+        {
+            string errorMessage = "My error message";
+            var customErrorObject = new CustomErrorTest { ErrorCode = -1 };
+            var customErrorObjectFactory = new CustomErrorTest { ErrorCode = -2 };
+            int counterPredicate = 0;
+            int counterErrorFactory = 0;
+            // Ok result
+            var ok = Result.Ok<int, CustomErrorTest>(42);
+
+            var result = ok.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(1, counterPredicate);
+            CheckResultOk(result, 42);
+
+            result = ok.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(2, counterPredicate);
+            Assert.AreEqual(0, counterErrorFactory);
+            CheckResultOk(result, 42);
+
+            result = ok.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(3, counterPredicate);
+            CheckResultFail(result, errorMessage, customErrorObjectFactory);
+
+            result = ok.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(4, counterPredicate);
+            Assert.AreEqual(1, counterErrorFactory);
+            CheckResultFail(result, errorMessage, customErrorObjectFactory);
+
+            // Warning result
+            var warning = Result.Warn<int, CustomErrorTest>(15, "My warning");
+
+            result = warning.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(5, counterPredicate);
+            CheckResultWarn(result, 15, "My warning");
+
+            result = warning.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(6, counterPredicate);
+            Assert.AreEqual(1, counterErrorFactory);
+            CheckResultWarn(result, 15, "My warning");
+
+            result = warning.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(7, counterPredicate);
+            CheckResultFail(result, errorMessage, customErrorObjectFactory);
+
+            result = warning.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(8, counterPredicate);
+            Assert.AreEqual(2, counterErrorFactory);
+            CheckResultFail(result, errorMessage, customErrorObjectFactory);
+
+            // Failure result
+            var failure = Result.Fail<int, CustomErrorTest>("My failure", customErrorObject);
+
+            result = failure.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(8, counterPredicate);
+            CheckResultFail(result, "My failure", customErrorObject);
+
+            result = failure.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return true;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(8, counterPredicate);
+            Assert.AreEqual(2, counterErrorFactory);
+            CheckResultFail(result, "My failure", customErrorObject);
+
+            result = failure.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                customErrorObjectFactory);
+            Assert.AreEqual(8, counterPredicate);
+            CheckResultFail(result, "My failure", customErrorObject);
+
+            result = failure.Ensure(
+                value =>
+                {
+                    ++counterPredicate;
+                    return false;
+                },
+                errorMessage,
+                () =>
+                {
+                    ++counterErrorFactory;
+                    return customErrorObjectFactory;
+                });
+            Assert.AreEqual(8, counterPredicate);
+            Assert.AreEqual(2, counterErrorFactory);
+            CheckResultFail(result, "My failure", customErrorObject);
+        }
+    }
+}
---
+++ src/Here/Result/Extensions/ResultExtensions.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Here.Results.Extensions
+{
+    /// <summary>
+    /// Extensions related to <see cref="Result"/>, <see cref="Result{T}"/>, <see cref="CustomResult{TError}"/> and <see cref="Result{T, TError}"/>.
+    /// </summary>
+    public static partial class ResultExtensions
+    {
+        #region Result
+
+        /// <summary>
+        /// Ensure that this <see cref="Result"/> fulfill the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <param name="result"><see cref="Result"/>.</param>
+        /// <param name="predicate">Predicate to match.</param>
+        /// <param name="errorMessage">The error message to use if the predicate is not fulfilled.</param>
+        /// <returns>A corresponding <see cref="Result"/>.</returns>
+        [PublicAPI]
+        public static Result Ensure(this Result result, [NotNull, InstantHandle] Func<bool> predicate, [NotNull] string errorMessage)
+        {
+            if (result.IsFailure)
+                return result;
+
+            if (!predicate())
+                return Result.Fail(errorMessage);
+
+            return result;
+        }
+
+        #endregion
+
+        #region Result<T>
+
+        /// <summary>
+        /// Ensure that this <see cref="Result{T}"/> fulfill the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the result value.</typeparam>
+        /// <param name="result"><see cref="Result{T}"/> to check.</param>
+        /// <param name="predicate">Predicate to match.</param>
+        /// <param name="errorMessage">The error message to use if the predicate is not fulfilled.</param>
+        /// <returns>A corresponding <see cref="Result{T}"/>.</returns>
+        public static Result<T> Ensure<T>(this Result<T> result, [NotNull, InstantHandle] Predicate<T> predicate, [NotNull] string errorMessage)
+        {
+            if (result.IsFailure)
+                return result;
+
+            if (!predicate(result.Value))
+                return Result.Fail<T>(errorMessage);
+
+            return result;
+        }
+
+        #endregion
+
+        #region CustomResult<TError>
+
+        /// <summary>
+        /// Ensure that this <see cref="CustomResult{TError}"/> fulfill the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="TError">Error type of the result.</typeparam>
+        /// <param name="result"><see cref="CustomResult{TError}"/>.</param>
+        /// <param name="predicate">Predicate to match.</param>
+        /// <param name="errorMessage">The error message to use if the predicate is not fulfilled.</param>
+        /// <param name="errorObject">Custom error object.</param>
+        /// <returns>A corresponding <see cref="CustomResult{TError}"/>.</returns>
+        public static CustomResult<TError> Ensure<TError>(this CustomResult<TError> result, 
+            [NotNull, InstantHandle] Func<bool> predicate, 
+            [NotNull] string errorMessage, 
+            [NotNull] TError errorObject)
+        {
+            if (result.IsFailure)
+                return result;
+
+            if (!predicate())
+                return Result.CustomFail(errorMessage, errorObject);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Ensure that this <see cref="CustomResult{TError}"/> fulfill the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="TError">Error type of the result.</typeparam>
+        /// <param name="result"><see cref="CustomResult{TError}"/>.</param>
+        /// <param name="predicate">Predicate to match.</param>
+        /// <param name="errorMessage">The error message to use if the predicate is not fulfilled.</param>
+        /// <param name="errorFactory">Function to create a custom error object.</param>
+        /// <returns>A corresponding <see cref="CustomResult{TError}"/>.</returns>
+        public static CustomResult<TError> Ensure<TError>(this CustomResult<TError> result,
+            [NotNull, InstantHandle] Func<bool> predicate,
+            [NotNull] string errorMessage,
+            [NotNull, InstantHandle] Func<TError> errorFactory)
+        {
+            if (result.IsFailure)
+                return result;
+
+            if (!predicate())
+                return Result.CustomFail(errorMessage, errorFactory());
+
+            return result;
+        }
+
+        #endregion
+
+        #region Result<T, TError>
+
+        /// <summary>
+        /// Ensure that this <see cref="Result{T, TError}"/> fulfill the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the result value.</typeparam>
+        /// <typeparam name="TError">Error type of the result.</typeparam>
+        /// <param name="result"><see cref="Result{T, TError}"/> to check.</param>
+        /// <param name="predicate">Predicate to match.</param>
+        /// <param name="errorMessage">The error message to use if the predicate is not fulfilled.</param>
+        /// <param name="errorObject">Custom error object.</param>
+        /// <returns>A corresponding <see cref="Result{T, TError}"/>.</returns>
+        public static Result<T, TError> Ensure<T, TError>(this Result<T, TError> result,
+            [NotNull, InstantHandle] Predicate<T> predicate,
+            [NotNull] string errorMessage,
+            [NotNull] TError errorObject)
+        {
+            if (result.IsFailure)
+                return result;
+
+            if (!predicate(result.Value))
+                return Result.Fail<T, TError>(errorMessage, errorObject);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Ensure that this <see cref="Result{T, TError}"/> fulfill the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the result value.</typeparam>
+        /// <typeparam name="TError">Error type of the result.</typeparam>
+        /// <param name="result"><see cref="Result{T, TError}"/> to check.</param>
+        /// <param name="predicate">Predicate to match.</param>
+        /// <param name="errorMessage">The error message to use if the predicate is not fulfilled.</param>
+        /// <param name="errorFactory">Function to create a custom error object.</param>
+        /// <returns>A corresponding <see cref="Result{T, TError}"/>.</returns>
+        public static Result<T, TError> Ensure<T, TError>(this Result<T, TError> result,
+            [NotNull, InstantHandle] Predicate<T> predicate,
+            [NotNull] string errorMessage,
+            [NotNull, InstantHandle] Func<TError> errorFactory)
+        {
+            if (result.IsFailure)
+                return result;
+
+            if (!predicate(result.Value))
+                return Result.Fail<T, TError>(errorMessage, errorFactory());
+
+            return result;
+        }
+
+        #endregion
+    }
+}
