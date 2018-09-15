4d2acd3047f4c23330cf57e259e5b39549bd2eed
Alexandre Rabérin <kernelith@live.fr>
2018-09-15 20:58:07 +0200

    Add a SuccessEquals method for each result type that allow to check that Result are equals and also success.

---
+++ src/Here/Result/ResultHelpers.cs
@@ -0,0 +1,55 @@
+﻿using JetBrains.Annotations;
+
+namespace Here.Results
+{
+    /// <summary>
+    /// Helpers to work with results.
+    /// </summary>
+    [PublicAPI]
+    public static class ResultHelpers
+    {
+        /// <summary>
+        /// Checks that both <see cref="Result"/> are equals and that they are successful.
+        /// </summary>
+        /// <param name="other"><see cref="Result"/> to compare.</param>
+        /// <returns>True if both <see cref="Result"/> are equals and successful.</returns>
+        [PublicAPI, Pure]
+        public static bool SuccessEquals(Result result1, Result result2)
+        {
+            return result1.SuccessEquals(result2);
+        }
+
+        /// <summary>
+        /// Checks that both <see cref="Result{T}"/> are equals and that they are successful.
+        /// </summary>
+        /// <param name="other"><see cref="Result{T}"/> to compare.</param>
+        /// <returns>True if both <see cref="Result{T}"/> are equals and successful.</returns>
+        [PublicAPI, Pure]
+        public static bool SuccessEquals<T>(Result<T> result1, Result<T> result2)
+        {
+            return result1.SuccessEquals(result2);
+        }
+
+        /// <summary>
+        /// Checks that both <see cref="CustomResult{TError}"/> are equals and that they are successful.
+        /// </summary>
+        /// <param name="other"><see cref="CustomResult{TError}"/> to compare.</param>
+        /// <returns>True if both <see cref="CustomResult{TError}"/> are equals and successful.</returns>
+        [PublicAPI, Pure]
+        public static bool SuccessEquals<TError>(CustomResult<TError> result1, CustomResult<TError> result2)
+        {
+            return result1.SuccessEquals(result2);
+        }
+
+        /// <summary>
+        /// Checks that both <see cref="Result{T, TError}"/> are equals and that they are successful.
+        /// </summary>
+        /// <param name="other"><see cref="Result{T, TError}"/> to compare.</param>
+        /// <returns>True if both <see cref="Result{T, TError}"/> are equals and successful.</returns>
+        [PublicAPI, Pure]
+        public static bool SuccessEquals<T, TError>(Result<T, TError> result1, Result<T, TError> result2)
+        {
+            return result1.SuccessEquals(result2);
+        }
+    }
+}
