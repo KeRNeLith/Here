718ad01ac6b3712fe30ea6a665caf23c12f9a6a2
Alexandre Rabérin <kernelith@live.fr>
2018-12-06 23:44:39 +0100

    Add OnSuccessOrFailure extensions for Results structures.

---
+++ src/Here.Tests/Result/OnSuccessOrFailure/OnSuccessExtensionsTests.cs
@@ -0,0 +1,204 @@
+﻿using NUnit.Framework;
+using Here.Extensions;
+
+namespace Here.Tests.Results
+{
+    /// <summary>
+    /// Tests for <see cref="Result"/>, <see cref="Result{T}"/>, <see cref="CustomResult{TError}"/> and <see cref="Result{T, TError}"/> extensions (On success or failure).
+    /// </summary>
+    [TestFixture]
+    internal class OnSuccessOrFailureExtensionsTests : ResultTestsBase
+    {
+        #region OnSuccessOrFailure Result => Value
+
+        [Test]
+        public void ResultOnSuccessOrFailureToValue()
+        {
+            #region Local function
+
+            void CheckOnSuccessOrFailure(Result result, bool treatWarningAsError, float expectedValue, bool expectSuccess)
+            {
+                int counterSuccess = 0;
+                int counterFailure = 0;
+                float res = result.OnSuccessOrFailure(
+                    r =>
+                    {
+                        ++counterSuccess;
+                        return 1.0f;
+                    },
+                    r =>
+                    {
+                        ++counterFailure;
+                        return -1.0f;
+                    },
+                    treatWarningAsError);
+                Assert.AreEqual(expectSuccess ? 1 : 0, counterSuccess);
+                Assert.AreEqual(expectSuccess ? 0 : 1, counterFailure);
+                Assert.AreEqual(expectedValue, res);
+            }
+
+            #endregion
+
+            // Ok result
+            var ok = Result.Ok();
+            CheckOnSuccessOrFailure(ok, false, 1.0f, true);
+            CheckOnSuccessOrFailure(ok, true, 1.0f, true);
+
+            // Warning result
+            var warning = Result.Warn("My warning");
+            CheckOnSuccessOrFailure(warning, false, 1.0f, true);
+            CheckOnSuccessOrFailure(warning, true, -1.0f, false);
+
+            // Failure result
+            var failure = Result.Fail("My failure");
+            CheckOnSuccessOrFailure(failure, false, -1.0f, false);
+            CheckOnSuccessOrFailure(failure, true, -1.0f, false);
+        }
+
+        #endregion
+
+        #region OnSuccessOrFailure Result<T> => Value
+
+        [Test]
+        public void ValueResultOnSuccessOrFailureToValue()
+        {
+            #region Local function
+
+            void CheckOnSuccessOrFailure(Result<int> result, bool treatWarningAsError, float expectedValue, bool expectSuccess)
+            {
+                int counterSuccess = 0;
+                int counterFailure = 0;
+                float res = result.OnSuccessOrFailure(
+                    r =>
+                    {
+                        ++counterSuccess;
+                        return r.Value + 1.0f;
+                    },
+                    r =>
+                    {
+                        ++counterFailure;
+                        return -1.0f;
+                    },
+                    treatWarningAsError);
+                Assert.AreEqual(expectSuccess ? 1 : 0, counterSuccess);
+                Assert.AreEqual(expectSuccess ? 0 : 1, counterFailure);
+                Assert.AreEqual(expectedValue, res);
+            }
+
+            #endregion
+
+            // Ok result
+            var ok = Result.Ok(1);
+            CheckOnSuccessOrFailure(ok, false, 2.0f, true);
+            CheckOnSuccessOrFailure(ok, true, 2.0f, true);
+
+            // Warning result
+            var warning = Result.Warn(2, "My warning");
+            CheckOnSuccessOrFailure(warning, false, 3.0f, true);
+            CheckOnSuccessOrFailure(warning, true, -1.0f, false);
+
+            // Failure result
+            var failure = Result.Fail<int>("My failure");
+            CheckOnSuccessOrFailure(failure, false, -1.0f, false);
+            CheckOnSuccessOrFailure(failure, true, -1.0f, false);
+        }
+
+        #endregion
+
+        #region OnSuccessOrFailure CustomResult<TError> => Value
+
+        [Test]
+        public void CustomResultOnSuccessOrFailureToValue()
+        {
+            #region Local function
+
+            void CheckOnSuccessOrFailure(CustomResult<CustomErrorTest> result, bool treatWarningAsError, float expectedValue, bool expectSuccess)
+            {
+                int counterSuccess = 0;
+                int counterFailure = 0;
+                float res = result.OnSuccessOrFailure(
+                    r =>
+                    {
+                        ++counterSuccess;
+                        return 1.0f;
+                    },
+                    r =>
+                    {
+                        ++counterFailure;
+                        return -1.0f;
+                    },
+                    treatWarningAsError);
+                Assert.AreEqual(expectSuccess ? 1 : 0, counterSuccess);
+                Assert.AreEqual(expectSuccess ? 0 : 1, counterFailure);
+                Assert.AreEqual(expectedValue, res);
+            }
+
+            #endregion
+
+            // Ok result
+            var ok = Result.CustomOk<CustomErrorTest>();
+            CheckOnSuccessOrFailure(ok, false, 1.0f, true);
+            CheckOnSuccessOrFailure(ok, true, 1.0f, true);
+
+            // Warning result
+            var warning = Result.CustomWarn<CustomErrorTest>("My warning");
+            CheckOnSuccessOrFailure(warning, false, 1.0f, true);
+            CheckOnSuccessOrFailure(warning, true, -1.0f, false);
+
+            // Failure result
+            var failure = Result.CustomFail("My failure", new CustomErrorTest { ErrorCode = -1 });
+            CheckOnSuccessOrFailure(failure, false, -1.0f, false);
+            CheckOnSuccessOrFailure(failure, true, -1.0f, false);
+        }
+
+        #endregion
+
+        #region OnSuccessOrFailure Result<T, TError> => Value
+
+        [Test]
+        public void ValueCustomResultOnSuccessOrFailureToValue()
+        {
+            #region Local function
+
+            void CheckOnSuccessOrFailure(Result<int, CustomErrorTest> result, bool treatWarningAsError, float expectedValue, bool expectSuccess)
+            {
+                int counterSuccess = 0;
+                int counterFailure = 0;
+                float res = result.OnSuccessOrFailure(
+                    r =>
+                    {
+                        ++counterSuccess;
+                        return r.Value + 1.0f;
+                    },
+                    r =>
+                    {
+                        ++counterFailure;
+                        return -1.0f;
+                    },
+                    treatWarningAsError);
+                Assert.AreEqual(expectSuccess ? 1 : 0, counterSuccess);
+                Assert.AreEqual(expectSuccess ? 0 : 1, counterFailure);
+                Assert.AreEqual(expectedValue, res);
+            }
+
+            #endregion
+
+            // Ok result
+            var ok = Result.Ok<int, CustomErrorTest>(1);
+            CheckOnSuccessOrFailure(ok, false, 2.0f, true);
+            CheckOnSuccessOrFailure(ok, true, 2.0f, true);
+
+            // Warning result
+            var warning = Result.Warn<int, CustomErrorTest>(2, "My warning");
+            CheckOnSuccessOrFailure(warning, false, 3.0f, true);
+            CheckOnSuccessOrFailure(warning, true, -1.0f, false);
+
+            // Failure result
+            var failure = Result.Fail<int, CustomErrorTest>("My failure", new CustomErrorTest { ErrorCode = -2 });
+            CheckOnSuccessOrFailure(failure, false, -1.0f, false);
+            CheckOnSuccessOrFailure(failure, true, -1.0f, false);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Here/Result/Extensions/ResultExtensions_OnSuccessOrFailure.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Here.Extensions
+{
+    /// <summary>
+    /// Extensions related to <see cref="Result"/>, <see cref="Result{T}"/>, <see cref="CustomResult{TError}"/> and <see cref="Result{T, TError}"/> (On success or failure).
+    /// </summary>
+    public static partial class ResultExtensions
+    {
+        #region Result
+
+        /// <summary>
+        /// Calls the <paramref name="onSuccess"/> function when the <paramref name="result"/> is a success,
+        /// otherwise the <paramref name="onFailure"/>.
+        /// </summary>
+        /// <typeparam name="TOut">Type of the output value.</typeparam>
+        /// <param name="result"><see cref="Result"/> to check.</param>
+        /// <param name="onSuccess">Function to run if the <see cref="Result"/> is a success.</param>
+        /// <param name="onFailure">Function to run if the <see cref="Result"/> is a failure.</param>
+        /// <param name="treatWarningAsError">Flag to indicate how to treat warning (By default as success).</param>
+        /// <returns>An output value.</returns>
+        [PublicAPI]
+        public static TOut OnSuccessOrFailure<TOut>(in this Result result,
+            [NotNull, InstantHandle] in Func<Result, TOut> onSuccess,
+            [NotNull, InstantHandle] in Func<Result, TOut> onFailure,
+            in bool treatWarningAsError = false)
+        {
+            if (IsConsideredSuccess(result, treatWarningAsError))
+                return onSuccess(result);
+            return onFailure(result);
+        }
+
+        #endregion
+
+        #region Result<T>
+
+        /// <summary>
+        /// Calls the <paramref name="onSuccess"/> function when the <paramref name="result"/> is a success,
+        /// otherwise the <paramref name="onFailure"/>.
+        /// </summary>
+        /// <typeparam name="TIn">Result input value type.</typeparam>
+        /// <typeparam name="TOut">Type of the output value.</typeparam>
+        /// <param name="result"><see cref="Result{TIn}"/> to check.</param>
+        /// <param name="onSuccess">Function to run if the <see cref="Result{TIn}"/> is a success.</param>
+        /// <param name="onFailure">Function to run if the <see cref="Result{TIn}"/> is a failure.</param>
+        /// <param name="treatWarningAsError">Flag to indicate how to treat warning (By default as success).</param>
+        /// <returns>An output value.</returns>
+        [PublicAPI]
+        public static TOut OnSuccessOrFailure<TIn, TOut>(in this Result<TIn> result,
+            [NotNull, InstantHandle] in Func<Result<TIn>, TOut> onSuccess,
+            [NotNull, InstantHandle] in Func<Result<TIn>, TOut> onFailure,
+            in bool treatWarningAsError = false)
+        {
+            if (IsConsideredSuccess(result, treatWarningAsError))
+                return onSuccess(result);
+            return onFailure(result);
+        }
+
+        #endregion
+
+        #region CustomResult<TError>
+
+        /// <summary>
+        /// Calls the <paramref name="onSuccess"/> function when the <paramref name="result"/> is a success,
+        /// otherwise the <paramref name="onFailure"/>.
+        /// </summary>
+        /// <typeparam name="TOut">Type of the output value.</typeparam>
+        /// <typeparam name="TError">Result custom error type.</typeparam>
+        /// <param name="result"><see cref="CustomResult{TError}"/> to check.</param>
+        /// <param name="onSuccess">Function to run if the <see cref="CustomResult{TError}"/> is a success.</param>
+        /// <param name="onFailure">Function to run if the <see cref="CustomResult{TError}"/> is a failure.</param>
+        /// <param name="treatWarningAsError">Flag to indicate how to treat warning (By default as success).</param>
+        /// <returns>An output value.</returns>
+        [PublicAPI]
+        public static TOut OnSuccessOrFailure<TOut, TError>(in this CustomResult<TError> result,
+            [NotNull, InstantHandle] in Func<CustomResult<TError>, TOut> onSuccess,
+            [NotNull, InstantHandle] in Func<CustomResult<TError>, TOut> onFailure,
+            in bool treatWarningAsError = false)
+        {
+            if (IsConsideredSuccess(result, treatWarningAsError))
+                return onSuccess(result);
+            return onFailure(result);
+        }
+
+        #endregion
+
+        #region Result<T, TError>
+
+        /// <summary>
+        /// Calls the <paramref name="onSuccess"/> function when the <paramref name="result"/> is a success,
+        /// otherwise the <paramref name="onFailure"/>.
+        /// </summary>
+        /// <typeparam name="TIn">Result input value type.</typeparam>
+        /// <typeparam name="TOut">Type of the output value.</typeparam>
+        /// <typeparam name="TError">Result custom error type.</typeparam>
+        /// <param name="result"><see cref="Result{TIn, TError}"/> to check.</param>
+        /// <param name="onSuccess">Function to run if the <see cref="Result{TIn, TError}"/> is a success.</param>
+        /// <param name="onFailure">Function to run if the <see cref="Result{TIn, TError}"/> is a failure.</param>
+        /// <param name="treatWarningAsError">Flag to indicate how to treat warning (By default as success).</param>
+        /// <returns>An output value.</returns>
+        [PublicAPI]
+        public static TOut OnSuccessOrFailure<TIn, TOut, TError>(in this Result<TIn, TError> result,
+            [NotNull, InstantHandle] in Func<Result<TIn, TError>, TOut> onSuccess,
+            [NotNull, InstantHandle] in Func<Result<TIn, TError>, TOut> onFailure,
+            in bool treatWarningAsError = false)
+        {
+            if (IsConsideredSuccess(result, treatWarningAsError))
+                return onSuccess(result);
+            return onFailure(result);
+        }
+
+        #endregion
+    }
+}
