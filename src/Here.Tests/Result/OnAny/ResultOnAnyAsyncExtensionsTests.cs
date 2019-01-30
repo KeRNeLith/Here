43c4fe8f5ab216dea2c1b9b12871619bf8cc5269
Alexandre Rabérin <kernelith@live.fr>
2019-01-30 23:23:22 +0100

    Add async OnAny extensions for Result.

---
+++ src/Here.Tests/Result/OnAny/ResultOnAnyAsyncExtensionsTests.cs
@@ -0,0 +1,103 @@
+﻿#if SUPPORTS_ASYNC
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using Here.Extensions;
+
+namespace Here.Tests.Results
+{
+    /// <summary>
+    /// Tests for <see cref="Result"/> asynchronous extensions.
+    /// </summary>
+    [TestFixture]
+    internal class ResultOnAnyAsyncExtensionsTests : ResultTestsBase
+    {
+        private static IEnumerable<TestCaseData> CreateOnAnyTestCases
+        {
+            [UsedImplicitly]
+            get
+            {
+                yield return new TestCaseData(Result.Ok());
+                yield return new TestCaseData(Result.Warn("My warning"));
+                yield return new TestCaseData(Result.Fail("My failure"));
+            }
+        }
+
+        [TestCaseSource(nameof(CreateOnAnyTestCases))]
+        public async Task ResultOnAnyAsync(Result result)
+        {
+            int counter = 0;
+
+            await result.OnAnyAsync(
+                res =>
+                {
+                    ++counter;
+                    return Task.Run(() => { /* Something */ });
+                });
+            Assert.AreEqual(1, counter);
+
+            string asyncRes = await result.OnAnyAsync(
+                res =>
+                {
+                    ++counter;
+                    return Task.FromResult("Test Async 0");
+                });
+            Assert.AreEqual(2, counter);
+            Assert.AreEqual("Test Async 0", asyncRes);
+
+            var taskResult = Task.FromResult(result);
+
+            await taskResult.OnAnyAsync(
+                () =>
+                {
+                    ++counter;
+                });
+            Assert.AreEqual(3, counter);
+
+            await taskResult.OnAnyAsync(
+                res =>
+                {
+                    ++counter;
+                });
+            Assert.AreEqual(4, counter);
+
+            asyncRes = await taskResult.OnAnyAsync(
+                res =>
+                {
+                    ++counter;
+                    return "Test Async 1";
+                });
+            Assert.AreEqual(5, counter);
+            Assert.AreEqual("Test Async 1", asyncRes);
+
+            await taskResult.OnAnyAsync(
+                res =>
+                {
+                    ++counter;
+                    return Task.Run(() => { /* Something */ });
+                });
+            Assert.AreEqual(6, counter);
+
+            asyncRes = await taskResult.OnAnyAsync(
+                res =>
+                {
+                    ++counter;
+                    return Task.FromResult("Test Async 2");
+                });
+            Assert.AreEqual(7, counter);
+            Assert.AreEqual("Test Async 2", asyncRes);
+
+            Assert.ThrowsAsync<ArgumentNullException>(() => result.OnAnyAsync(null));
+            Assert.ThrowsAsync<ArgumentNullException>(() => taskResult.OnAnyAsync(null));
+            Assert.ThrowsAsync<ArgumentNullException>(() => result.OnAnyAsync((Func<Result, Task<int>>)null));
+            Assert.ThrowsAsync<ArgumentNullException>(() => taskResult.OnAnyAsync((Func<Result, Task<int>>)null));
+            Assert.ThrowsAsync<ArgumentNullException>(() => taskResult.OnAnyAsync((Action)null));
+            Assert.ThrowsAsync<ArgumentNullException>(() => taskResult.OnAnyAsync((Action<Result>)null));
+            Assert.ThrowsAsync<ArgumentNullException>(() => taskResult.OnAnyAsync((Func<Result, int>)null));
+        }
+    }
+}
+
+#endif
---
+++ src/Here/Result/Extensions/ResultExtensions_OnAnyAsync.cs
@@ -0,0 +1,161 @@
+﻿#if SUPPORTS_ASYNC
+using System;
+#if SUPPORTS_AGGRESSIVE_INLINING
+using System.Runtime.CompilerServices;
+#endif
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Here.Extensions
+{
+    /// <summary>
+    /// Asynchronous extensions related to <see cref="Result"/>, <see cref="Result{T}"/>, <see cref="CustomResult{TError}"/> and <see cref="Result{T, TError}"/> (On any).
+    /// </summary>
+    public static partial class ResultAsyncExtensions
+    {
+        #region Result
+
+        /// <summary>
+        /// Calls the <paramref name="onAny"/> action all the time.
+        /// </summary>
+        /// <param name="result"><see cref="Result"/>.</param>
+        /// <param name="onAny">Action to run.</param>
+        /// <param name="continueOnCapturedContext">Indicates if asynchronous execution should continue on captured context.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        /// <exception cref="ArgumentNullException">If the <paramref name="onAny"/> is null.</exception>
+        [PublicAPI]
+#if SUPPORTS_AGGRESSIVE_INLINING
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        public static async Task OnAnyAsync(this Result result,
+            [NotNull, InstantHandle] Func<Result, Task> onAny,
+            bool continueOnCapturedContext = false)
+        {
+            Throw.IfArgumentNull(onAny, nameof(onAny));
+
+            await onAny(result).ConfigureAwait(continueOnCapturedContext);
+        }
+
+        /// <summary>
+        /// Calls the <paramref name="onAny"/> action all the time.
+        /// </summary>
+        /// <typeparam name="TOut">Type of the output value.</typeparam>
+        /// <param name="result"><see cref="Result"/>.</param>
+        /// <param name="onAny">Action to run.</param>
+        /// <param name="continueOnCapturedContext">Indicates if asynchronous execution should continue on captured context.</param>
+        /// <returns>A <see cref="Task{TOut}"/>.</returns>
+        /// <exception cref="ArgumentNullException">If the <paramref name="onAny"/> is null.</exception>
+        [PublicAPI]
+#if SUPPORTS_AGGRESSIVE_INLINING
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        public static async Task<TOut> OnAnyAsync<TOut>(this Result result, 
+            [NotNull, InstantHandle] Func<Result, Task<TOut>> onAny,
+            bool continueOnCapturedContext = false)
+        {
+            Throw.IfArgumentNull(onAny, nameof(onAny));
+
+            return await onAny(result).ConfigureAwait(continueOnCapturedContext);
+        }
+
+        /// <summary>
+        /// Calls the <paramref name="onAny"/> action all the time.
+        /// </summary>
+        /// <param name="resultTask"><see cref="Task{Result}"/>.</param>
+        /// <param name="onAny">Action to run.</param>
+        /// <param name="continueOnCapturedContext">Indicates if asynchronous execution should continue on captured context.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        /// <exception cref="ArgumentNullException">If the <paramref name="onAny"/> is null.</exception>
+        [PublicAPI]
+        public static async Task OnAnyAsync([NotNull] this Task<Result> resultTask,
+            [NotNull, InstantHandle] Action onAny,
+            bool continueOnCapturedContext = false)
+        {
+            Throw.IfArgumentNull(onAny, nameof(onAny));
+
+            Result result = await resultTask.ConfigureAwait(continueOnCapturedContext);
+            result.OnAny(onAny);
+        }
+
+        /// <summary>
+        /// Calls the <paramref name="onAny"/> action all the time.
+        /// </summary>
+        /// <param name="resultTask"><see cref="Task{Result}"/>.</param>
+        /// <param name="onAny">Action to run.</param>
+        /// <param name="continueOnCapturedContext">Indicates if asynchronous execution should continue on captured context.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        /// <exception cref="ArgumentNullException">If the <paramref name="onAny"/> is null.</exception>
+        [PublicAPI]
+        public static async Task OnAnyAsync([NotNull] this Task<Result> resultTask,
+            [NotNull, InstantHandle] Action<Result> onAny,
+            bool continueOnCapturedContext = false)
+        {
+            Throw.IfArgumentNull(onAny, nameof(onAny));
+
+            Result result = await resultTask.ConfigureAwait(continueOnCapturedContext);
+            result.OnAny(onAny);
+        }
+
+        /// <summary>
+        /// Calls the <paramref name="onAny"/> action all the time.
+        /// </summary>
+        /// <typeparam name="TOut">Type of the output value.</typeparam>
+        /// <param name="resultTask"><see cref="Task{Result}"/>.</param>
+        /// <param name="onAny">Action to run.</param>
+        /// <param name="continueOnCapturedContext">Indicates if asynchronous execution should continue on captured context.</param>
+        /// <returns>A <see cref="Task{TOut}"/>.</returns>
+        /// <exception cref="ArgumentNullException">If the <paramref name="onAny"/> is null.</exception>
+        [PublicAPI]
+        public static async Task<TOut> OnAnyAsync<TOut>([NotNull] this Task<Result> resultTask,
+            [NotNull, InstantHandle] Func<Result, TOut> onAny,
+            bool continueOnCapturedContext = false)
+        {
+            Throw.IfArgumentNull(onAny, nameof(onAny));
+
+            Result result = await resultTask.ConfigureAwait(continueOnCapturedContext);
+            return result.OnAny(onAny);
+        }
+
+        /// <summary>
+        /// Calls the <paramref name="onAny"/> action all the time.
+        /// </summary>
+        /// <param name="resultTask"><see cref="Task{Result}"/>.</param>
+        /// <param name="onAny">Action to run.</param>
+        /// <param name="continueOnCapturedContext">Indicates if asynchronous execution should continue on captured context.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        /// <exception cref="ArgumentNullException">If the <paramref name="onAny"/> is null.</exception>
+        [PublicAPI]
+        public static async Task OnAnyAsync([NotNull] this Task<Result> resultTask,
+            [NotNull, InstantHandle] Func<Result, Task> onAny,
+            bool continueOnCapturedContext = false)
+        {
+            Throw.IfArgumentNull(onAny, nameof(onAny));
+
+            Result result = await resultTask.ConfigureAwait(continueOnCapturedContext);
+            await onAny(result).ConfigureAwait(continueOnCapturedContext);
+        }
+
+        /// <summary>
+        /// Calls the <paramref name="onAny"/> action all the time.
+        /// </summary>
+        /// <typeparam name="TOut">Type of the output value.</typeparam>
+        /// <param name="resultTask"><see cref="Task{Result}"/>.</param>
+        /// <param name="onAny">Action to run.</param>
+        /// <param name="continueOnCapturedContext">Indicates if asynchronous execution should continue on captured context.</param>
+        /// <returns>A <see cref="Task{TOut}"/>.</returns>
+        /// <exception cref="ArgumentNullException">If the <paramref name="onAny"/> is null.</exception>
+        [PublicAPI]
+        public static async Task<TOut> OnAnyAsync<TOut>([NotNull] this Task<Result> resultTask, 
+            [NotNull, InstantHandle] Func<Result, Task<TOut>> onAny, 
+            bool continueOnCapturedContext = false)
+        {
+            Throw.IfArgumentNull(onAny, nameof(onAny));
+
+            Result result = await resultTask.ConfigureAwait(continueOnCapturedContext);
+            return await onAny(result).ConfigureAwait(continueOnCapturedContext);
+        }
+
+        #endregion
+    }
+}
+#endif
