69a5cef2aed59bcfbbd448e20b5178acc221af63
Alexandre Rabérin <kernelith@live.fr>
2020-09-02 20:41:23 +0200

    Fix package release builds.

--- build/setupBuild.ps1
+++ build/setupBuild.ps1
@@ -18,7 +18,7 @@ function UpdateAllPackagesGeneration()
 function UpdateDeployBuild()
 {
     # Update the package generation props to enable package generation of the right package
-    $genPackagesFilePath = "./build/SetupBuild.props";
+    $genPackagesFilePath = "./build/BuildSetup.props";
     $genPackagesContent = Get-Content $genPackagesFilePath;
     $newGenPackagesContent = $genPackagesContent -replace "false","true";
     $newGenPackagesContent | Set-Content $genPackagesFilePath;
@@ -29,17 +29,19 @@ function UpdateDeployBuild()
 if ($env:APPVEYOR_REPO_TAG -eq "true")
 {
     UpdateDeployBuild;
+    $env:Build_Version = "$($env:APPVEYOR_REPO_TAG_NAME.Replace('v', ''))";
+    $env:Build_Assembly_Version = $env:Build_Version;
     $env:IsFullIntegrationBuild = $false;   # Run only tests on deploy builds (not coverage, etc.)
 }
 else
 {
+    $env:Build_Version = "$($env:APPVEYOR_BUILD_VERSION)";
+    $env:Build_Assembly_Version = "$env:Build_Version" -replace "\-.*","";
     $env:IsFullIntegrationBuild = "$env:APPVEYOR_PULL_REQUEST_NUMBER" -eq "" -And $env:Configuration -eq "Release";
 }
 
 UpdateAllPackagesGeneration;
-$env:Build_Version = "$($env:APPVEYOR_BUILD_VERSION)";
 $env:Release_Name = $env:Build_Version;
-$env:Build_Assembly_Version = "$env:Build_Version" -replace "\-.*","";
 
 "Building version: $env:Build_Version";
 "Building assembly version: $env:Build_Assembly_Version";
